814adf49cae992ccc40b6e7c3b04e549fe0fd7f1
Alex Harker <ajharker@gmail.com>
2022-01-17 15:05:14 +0000

    Update build script

--- scripts/dist-mac-main.sh
+++ scripts/dist-mac-main.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 
-EXPORT_PATH="$BASEDIR/../build-mac/"
+EXPORT_PATH="$BASEDIR/../build-mac/HISSTools_Granular"
 AUDIO_PATH="$HOME/Library/Audio/Plug-Ins"
 
 if [ ! -d "$EXPORT_PATH" ]
@@ -14,6 +14,8 @@ if [ ! -d "$EXPORT_PATH/OSX" ]
 then
   mkdir "$EXPORT_PATH/OSX"
 fi
+
+cp "$BASEDIR/../manual/HISSTools_Granular_User_Guide.pdf" "$EXPORT_PATH/" || exit 1
 
 "$BASEDIR/dist-mac-notarize.sh" "$EXPORT_PATH" HISSToolsGranular.component "$AUDIO_PATH/Components" || exit 1
 "$BASEDIR/dist-mac-notarize.sh" "$EXPORT_PATH" HISSToolsGranular.vst "$AUDIO_PATH/VST" || exit 1
