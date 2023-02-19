0f6cb9df7b7373a8d4dfd83fc87b998b2cfbd0ca
Alex Harker <ajharker@gmail.com>
2023-02-19 16:25:09 +0000

    Sign only locally in the project and force sign when notarizing

--- scripts/dist-mac-notarize.sh
+++ scripts/dist-mac-notarize.sh
@@ -16,6 +16,8 @@ ZIP1_PATH="$EXPORT_PATH/$NOTIFY_NAME.zip"
 ZIP2_PATH="$EXPORT_PATH/OSX/$PLUG_NAME.zip"
 
 # Create a ZIP archive suitable for notarytool
+
+codesign --force --timestamp --sign "Developer ID Application" "$PLUG_PATH"
 /usr/bin/ditto -c -k --keepParent "$PLUG_PATH" "$ZIP1_PATH" || exit 1
 
 # Notarise
